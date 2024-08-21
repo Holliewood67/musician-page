ef29aa105ccf339bac34e27132e0e8a48407caa1
Holliewood67 <ethancantrell67@gmail.com>
2024-08-21 01:13:51 -0500

    added Andrew Live to monster list

--- src/data/monsters.js
+++ src/data/monsters.js
@@ -10,7 +10,7 @@ export default {
                 `For the past 3 years, Ethan has been hosting open mics and showcases called "Musical Monsters," fostering a community of musicians and bringing new talent to the forefront. His experience in these settings has allowed him to develop a deep connection with audiences, making each performance memorable.`,
                 `Known for his refined yet high-energy performances, Ethan knows how to engage an audience and keep them listening. Whether it’s an intimate venue or a lively bar, he’s ready to bring his passion for music to the stage, ensuring an experience that resonates with listeners.`
             ],
-            spotifyUrl: "3TrHIZ92sj1YvABKrcJTQE?utm_source=generator",
+            spotifyUrl: "3TrHIZ92sj1YvABKrcJTQE?utm_source=generator&theme=0",
             fbLink: "https://www.facebook.com/ECMusician",
             igLink: "https://www.instagram.com/ethancantrellmusician/",
             ttLink: "https://www.tiktok.com/@ethancantrellmusician",
@@ -27,14 +27,29 @@ export default {
                 `Jeff's music is deeply personal, reflecting his own experiences and emotions, and he pours this raw energy into every performance. His songs resonate with authenticity, drawing listeners into the stories he tells through his lyrics and melodies.`,
                 `Currently, Jeff is dedicated to improving his skills and working towards releasing his first album by the end of the year. His journey in music is just beginning, but his passion and commitment already make him a compelling force in the folk punk scene, leaving a lasting impact on everyone who hears him play.`
             ],
-            spotifyUrl: "7jV8Cj5bqsja5xOXainG8i?utm_source=generator",
+            spotifyUrl: "7jV8Cj5bqsja5xOXainG8i?utm_source=generator&theme=0",
             fbLink: "https://www.facebook.com/profile.php?id=695022605",
             igLink: "",
             ttLink: "",
             ytLink: "",
             bcLink: "",
-
-
+        },
+        {
+            name: "Andrew Live",
+            pics: ["andrew-live-1", "andrew-live-2"],
+            urlName: "andrew-live",
+            bioText: [
+                `has been a staple in the Tulsa, OK music scene for 27 years, playing guitar and singing at venues across the tri-state area. He currently hosts a 21+ open mic on Wednesdays at Smitty's on Main St. Jenks and an all-ages open mic on Tuesdays at Mother Road Market, alongside his solo performances at various dinner services and bars around Tulsa and its surrounding areas.`,
+                `Andrew is eager to expand his reach by adding new and exciting venues to his roster and setting up more local music showcases. His ambition also includes broadening his bookings to other areas, bringing his dynamic performances to a wider audience.`,
+                `Some of the recent venues where Andrew has performed include Good Day Tulsa, The Hunt Club, The District in Owasso, and The Patriot Golf Club. His performances have also graced CJ Moloney's, Sidecar on Cherry Street and in Jenks, and notable events like Tulsa Mayfest and the Tulsa State Fair. Whether it’s a lively bar or a sophisticated dinner service, Andrew knows how to keep the energy alive.`,
+                `Influenced by '90s alternative rock, Andrew Live’s sets now also feature elements of hip hop and R&B, covering over three hours of music. With 30 original songs under his belt, an extensive playlist, and professional sound and promotional equipment, Andrew ensures a seamless and entertaining experience for any venue. His ultimate goal is to drive sales, entertain the crowd, and create an unforgettable atmosphere.`
+            ],
+            spotifyUrl: "4OEzL5wrgHWiaEIbWbNhBT?utm_source=generator&theme=0",
+            fbLink: "https://www.facebook.com/AndrewLive143",
+            igLink: "https://www.instagram.com/andrew_live143/",
+            ttLink: "",
+            ytLink: "https://www.youtube.com/@andrewlive3428",
+            bcLink: "",
         },
         // {
         //     name: "",
